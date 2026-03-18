d747e12b12f136a569a2e0fe61f48c00a00a1631
Shardul9999 <shardulhingane16@gmail.com>
2026-03-18 13:15:45 +0530

    Password Logic remanning

---
+++ main.py
@@ -0,0 +1,98 @@
+from tkinter import *
+from tkinter import messagebox 
+import random
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+  password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+  password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+  
+pass_length = password_length_entry.get()
+
+print(f"Your password is: {password}")
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(message="Please fill all the credentials")
+    
+    else:
+        is_okay = messagebox.askokcancel(title= website, message= f"Entered Details are : \nEmail: {email} "f"\nPassword: {password} \nSave or not?")
+        
+        with open("data.txt", "a") as Data_file:
+            Data_file.write(f"{website} | {email} | {password}")
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
+        
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100,image=logo_img)
+#canvas.pack()
+canvas.grid(row=0, column=1)
+ 
+#Labels
+website_label = Label(text = "Website:")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text = "Email/Username:")
+email_label.grid(row=2, column=0)
+
+pass_length_label = Label(text = "Password Length:")
+pass_length_label.grid(row=3, column=0)
+
+password_label= Label(text = "password:")
+password_label.grid(row=4, column=0) 
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.focus()  
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "example@gmail.com") 
+
+password_length_entry = Entry(width=15)
+password_length_entry.grid(row=3, column=1, sticky="EW")
+
+password_entry = Entry(width=21)
+password_entry.grid(row=4, column=1, sticky="EW")
+
+# Buttons
+generate_password_button = Button(text="Generate Password") 
+generate_password_button.grid(row=4, column=2, sticky="EW")
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=5, column=1, columnspan=2, sticky="EW")
+
+window.mainloop()
